cc24da4d4e32b418368d090dfef8ad03fbbf5c15
Raafat-itu <raafat@innovation-through-understanding.de>
2024-04-15 18:18:22 +0200

    changed button icon in QuizzesPanel.tsx file

--- packages/frontend/src/components/quizzes-panel/QuizzesPanel.tsx
+++ packages/frontend/src/components/quizzes-panel/QuizzesPanel.tsx
@@ -5,7 +5,7 @@ import { useStatefulActor } from "ts-actors-react";
 import { Quiz, User, Id, toId } from "@recapp/models";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import { ArrowUp, Plus } from "react-bootstrap-icons";
+import { Download, Plus } from "react-bootstrap-icons";
 import { ArchiveQuizMessage } from "../../actors/LocalUserActor";
 import { QuizCard } from "./QuizCard";
 import { ShareModal } from "../modals/ShareModal";
@@ -13,95 +13,95 @@ import { YesNoModal } from "../modals/YesNoModal";
 import { QuizImportModal } from "../modals/QuizImportModal";
 
 export const QuizzesPanel: React.FC = () => {
-	const nav = useNavigate();
-	const [shareModal, setShareModal] = useState("");
-	const [deleteModal, setDeleteModal] = useState(toId(""));
-	const [importModal, setImportModal] = useState(false);
-	const [quizzes, setQuizzes] = useState<Array<Partial<Quiz>>>();
-	const updateCounterRef = useRef<number>(0);
-	const [state, tryLocalUserActor] = useStatefulActor<{
-		user: User | undefined;
-		quizzes: Map<Id, Partial<Quiz>>;
-		updateCounter: number;
-	}>("LocalUser");
-	useEffect(() => {
-		const counter = state.map(s => s.updateCounter).orElse(0);
-		if (counter === updateCounterRef.current) {
-			return;
-		}
-		updateCounterRef.current = counter;
-		const q: Array<Partial<Quiz>> = state.map(s => Array.from(s.quizzes.values())).orElse([]);
-		setQuizzes(
-			q.toSorted((a, b) => {
-				// eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-				return b.updated?.value! - a.updated?.value!;
-			})
-		);
-	}, [state]);
+    const nav = useNavigate();
+    const [shareModal, setShareModal] = useState("");
+    const [deleteModal, setDeleteModal] = useState(toId(""));
+    const [importModal, setImportModal] = useState(false);
+    const [quizzes, setQuizzes] = useState<Array<Partial<Quiz>>>();
+    const updateCounterRef = useRef<number>(0);
+    const [state, tryLocalUserActor] = useStatefulActor<{
+        user: User | undefined;
+        quizzes: Map<Id, Partial<Quiz>>;
+        updateCounter: number;
+    }>("LocalUser");
+    useEffect(() => {
+        const counter = state.map(s => s.updateCounter).orElse(0);
+        if (counter === updateCounterRef.current) {
+            return;
+        }
+        updateCounterRef.current = counter;
+        const q: Array<Partial<Quiz>> = state.map(s => Array.from(s.quizzes.values())).orElse([]);
+        setQuizzes(
+            q.toSorted((a, b) => {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+                return b.updated?.value! - a.updated?.value!;
+            })
+        );
+    }, [state]);
 
-	const deleteAllowed = (quiz: Partial<Quiz>): true | undefined => {
-		const isAdmin = state
-			.map(s => s.user)
-			.map(u => u?.role === "ADMIN")
-			.orElse(false);
-		const isTeacher = state
-			.map(s => s.user)
-			.map(u => (u?.uid && quiz.teachers?.includes(u?.uid)) ?? false)
-			.orElse(false);
-		return isAdmin || isTeacher ? true : undefined;
-	};
+    const deleteAllowed = (quiz: Partial<Quiz>): true | undefined => {
+        const isAdmin = state
+            .map(s => s.user)
+            .map(u => u?.role === "ADMIN")
+            .orElse(false);
+        const isTeacher = state
+            .map(s => s.user)
+            .map(u => (u?.uid && quiz.teachers?.includes(u?.uid)) ?? false)
+            .orElse(false);
+        return isAdmin || isTeacher ? true : undefined;
+    };
 
-	const deleteQuestion = () => {
-		tryLocalUserActor.forEach(q => q.send(q, new ArchiveQuizMessage(deleteModal)));
-		setDeleteModal(toId(""));
-	};
+    const deleteQuestion = () => {
+        tryLocalUserActor.forEach(q => q.send(q, new ArchiveQuizMessage(deleteModal)));
+        setDeleteModal(toId(""));
+    };
 
-	return (
-		<Container fluid className="p-0">
-			<div className="d-flex justify-content-end">
-				<Button
-					className="ps-1 d-flex justify-content-center align-items-center mb-3"
-					onClick={() => nav({ pathname: "/Dashboard/CreateQuiz" })}
-				>
-					<Plus size={28} />
-					<Trans id="button-new-quiz" />
-				</Button>
-				&nbsp;
-				<Button
-					className="ps-1 d-flex justify-content-center align-items-center mb-3"
-					onClick={() => setImportModal(true)}
-				>
-					<ArrowUp size={28} />
-					<Trans id="button-import-quiz" />
-				</Button>
-			</div>
-			<YesNoModal
-				show={!!deleteModal}
-				titleId="archive-quiz-title"
-				textId="archive-quiz-text"
-				onClose={() => setDeleteModal(toId(""))}
-				onSubmit={deleteQuestion}
-			/>
-			<QuizImportModal show={importModal} onClose={() => setImportModal(false)} />
+    return (
+        <Container fluid className="p-0">
+            <div className="d-flex justify-content-end">
+                <Button
+                    className="ps-1 d-flex justify-content-center align-items-center mb-3"
+                    onClick={() => nav({ pathname: "/Dashboard/CreateQuiz" })}
+                >
+                    <Plus size={28} />
+                    <Trans id="button-new-quiz" />
+                </Button>
+                &nbsp;
+                <Button
+                    className="ps-1 d-flex justify-content-center align-items-center mb-3"
+                    onClick={() => setImportModal(true)}
+                >
+                    <Download size={20} className="mx-2" />
+                    <Trans id="button-import-quiz" />
+                </Button>
+            </div>
+            <YesNoModal
+                show={!!deleteModal}
+                titleId="archive-quiz-title"
+                textId="archive-quiz-text"
+                onClose={() => setDeleteModal(toId(""))}
+                onSubmit={deleteQuestion}
+            />
+            <QuizImportModal show={importModal} onClose={() => setImportModal(false)} />
 
-			<ShareModal quizLink={shareModal} onClose={() => setShareModal("")} />
+            <ShareModal quizLink={shareModal} onClose={() => setShareModal("")} />
 
-			<Container
-				fluid
-				className="border-2 border-top"
-				// style={{ maxHeight: "70vh", overflowY: "auto" }}
-			>
-				{(quizzes ?? []).map(q => {
-					return (
-						<QuizCard
-							key={q.uid}
-							quiz={q}
-							onShare={() => setShareModal(q.uniqueLink!)}
-							onDelete={deleteAllowed(q) && (() => setDeleteModal(q.uid!))}
-						/>
-					);
-				})}
-			</Container>
-		</Container>
-	);
+            <Container
+                fluid
+                className="border-2 border-top"
+                // style={{ maxHeight: "70vh", overflowY: "auto" }}
+            >
+                {(quizzes ?? []).map(q => {
+                    return (
+                        <QuizCard
+                            key={q.uid}
+                            quiz={q}
+                            onShare={() => setShareModal(q.uniqueLink!)}
+                            onDelete={deleteAllowed(q) && (() => setDeleteModal(q.uid!))}
+                        />
+                    );
+                })}
+            </Container>
+        </Container>
+    );
 };

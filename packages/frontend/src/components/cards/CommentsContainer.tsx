f5b8316150f00fc061a9203efc3c60d3afe18445
Raafat-itu <raafat@innovation-through-understanding.de>
2024-04-17 21:32:44 +0200

    added toggle button to the CommentsContainer component

--- packages/frontend/src/components/cards/CommentsContainer.tsx
+++ packages/frontend/src/components/cards/CommentsContainer.tsx
@@ -1,38 +1,51 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import Button from "react-bootstrap/Button";
 import { Plus } from "react-bootstrap-icons";
 import { Trans } from "@lingui/react";
 
 type Props = {
-	showCommentArea: boolean;
-	onClickAddComment: () => void;
+    showCommentArea: boolean;
+    onClickAddComment: () => void;
 } & PropsWithChildren;
 
 export const CommentsContainer = (props: Props) => {
-	if (!props.showCommentArea) {
-		return null;
-	}
-	return (
-		<div
-			className="d-flex align-items-center border"
-			style={{
-				maxHeight: "19rem",
-				overflowY: "hidden",
-				overflowX: "auto",
-				backgroundColor: "#f5f5f5",
-				minHeight: "18rem",
-			}}
-		>
-			<div className="d-flex flex-column justify-content-center align-items-center p-4">
-				<Button variant="secondary" onClick={props.onClickAddComment}>
-					<Plus size={100} />
-				</Button>
-				<span style={{ width: 140, fontSize: "1.2rem", textAlign: "center" }}>
-					<Trans id="comment-row-new-comment-button" />
-				</span>
-			</div>
+    const [open, setOpen] = useState(false);
 
-			{props.children}
-		</div>
-	);
+    if (!props.showCommentArea) {
+        return null;
+    }
+
+    return (
+        <>
+            <div className="d-flex">
+                <Button className="flex-fill" onClick={() => setOpen(prev => !prev)}>
+                    <Trans id="comments-container.toggle-button.label" />
+                </Button>
+            </div>
+            {open ? (
+                <div
+                    className="d-flex align-items-center border"
+                    style={{
+                        maxHeight: "19rem",
+                        // height: "19rem",
+                        overflowY: "hidden",
+                        overflowX: "auto",
+                        backgroundColor: "#f5f5f5",
+                        minHeight: "18rem",
+                    }}
+                >
+                    <div className="d-flex flex-column justify-content-center align-items-center p-4">
+                        <Button variant="secondary" onClick={props.onClickAddComment}>
+                            <Plus size={100} />
+                        </Button>
+                        <span style={{ width: 140, fontSize: "1.2rem", textAlign: "center" }}>
+                            <Trans id="comment-row-new-comment-button" />
+                        </span>
+                    </div>
+
+                    {props.children}
+                </div>
+            ) : null}
+        </>
+    );
 };

e73da197c8b61192dac83871226b85cb4fc68c22
RaafatDev <raafat.dev@gmail.com>
2024-09-25 14:05:43 +0200

    refactored QuestionBarChart and QuizBarChart components to fix the position ot the svg's text element when zooming is active in presentation-mode

--- packages/frontend/src/components/quiz-tabs/quiz-bar/QuizBarChart.tsx
+++ packages/frontend/src/components/quiz-tabs/quiz-bar/QuizBarChart.tsx
@@ -9,12 +9,24 @@ interface Props {
 	maxValue: number;
 }
 
+
+// important information regarding the svg's <text></text> element:  
+// 1. by default the "x" attribute for the <text></text> element is where the left edge of the text starts
+//    to change the meaning of the "x" attribute use the "text-anchor" // https://developer.mozilla.org/en-US/docs/Web/CSS/text-anchor
+// 2. the "y" attribute for the <text></text> is not the top !!!, is is the bottom of the text
+//    to change the meaning of the "y" attribute use the "dominant-baseline" // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/dominant-baseline
+
 export const QuizBarChart = (props: Props) => {
 	const barWidth = 400; // Height of the chart area
 	const hasMaxValue = props.maxValue > 0;
 
 	return (
-		<svg viewBox="0 0 400 24" fill="blue" width="100%" height="20">
+		<svg 
+			viewBox="0 0 400 24" 
+			fill="blue" 
+			width="100%" 
+			height="20" 
+		>
 			<>
 				<Fragment key={0}>
 					<QuizBar y={0 * 25} width={barWidth} color={"grey"} />
@@ -29,20 +41,47 @@ export const QuizBarChart = (props: Props) => {
 							color={WRONG_COLOR}
 						/>
 					) : null}
-					<text x="10" y={0 * 25 + 19} z="10" style={{ fill: "white", fontSize: 20 }}>
-						{CHECK_SYMBOL} {props.correct} (
+
+					{/* <text x="10" y={0 * 25 + 19} z="10" style={{ fill: "white", fontSize: 20 }}> */}
+					<text 
+						z="10" 
+						x="0" 
+						textAnchor="start"// to change the meaning of the "x" attribute
+						y="0"
+						dominantBaseline="text-before-edge" // to change the meaning of the "y" attribute
+						style={{ fill: "white", fontSize: 20 }}
+					>
+						&nbsp;{CHECK_SYMBOL} {props.correct} (
 						{hasMaxValue ? `${((props.correct / props.maxValue) * 100.0).toFixed(1)}%` : ""})
 					</text>
-					<text x="190" y={0 * 25 + 19} z="10" style={{ fill: "white", fontSize: 20 }}>
+					{/* <text x="190" y={0 * 25 + 19} z="10" style={{ fill: "white", fontSize: 20 }}> */}
+					<text 
+						z="10" 
+						x="50%" 
+						textAnchor="middle" // to change the meaning of the "x" attribute
+						y="0"
+						dominantBaseline="text-before-edge" // to change the meaning of the "y" attribute
+						style={{ fill: "white", fontSize: 20 }}
+					>
 						{hasMaxValue ? `(${props.maxValue - props.correct - props.wrong})` : ""}
 					</text>
+                    {/* <text
+                        x={370 - (props.maxValue - props.correct).toString().length * 10}
+                        y={0 * 25 + 19}
+                        z="10"
+                        style={{ fill: "white", fontSize: 20, textAlign: "right" }}
+                    >
+                        {props.wrong} {X_SYMBOL}
+                    </text> */}
 					<text
-						x={370 - (props.maxValue - props.correct).toString().length * 10}
-						y={0 * 25 + 19}
 						z="10"
-						style={{ fill: "white", fontSize: 20, textAlign: "right" }}
+						x="100%"
+						textAnchor="end" // to change the meaning of the "x" attribute
+						y="0"
+						dominantBaseline="text-before-edge" // to change the meaning of the "y" attribute
+						style={{ fill: "white", fontSize: 20 }}
 					>
-						{props.wrong} {X_SYMBOL}
+						{props.wrong} {X_SYMBOL}&nbsp;
 					</text>
 				</Fragment>
 			</>

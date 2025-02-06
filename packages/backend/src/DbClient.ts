5e2603b9a111869f2c570d67daa7b67cda01da14
Sebastian Hanß <sebastian@hanss.info>
2025-02-06 09:06:29 +0100

    Revert "Added dockerfiles for frontend and backend containers. Adjusted docker-compose for production serving. Fixed bugs that only occured when running inside docker."
    This reverts commit 1d32821df1cc00e128e50d3384bcd3bbdd65ba40.

--- packages/backend/src/DbClient.ts
+++ packages/backend/src/DbClient.ts
@@ -2,42 +2,39 @@ import { Auth, Db, MongoClient } from "mongodb";
 import { Service } from "typedi";
 
 export const createConnection = async (): Promise<MongoClient> => {
-  const { MONGODB_PORT, MONGODB_USER, MONGODB_PW, MONGODB_HOST } = process.env;
-  const client = new MongoClient(
-    `mongodb://${MONGODB_HOST ?? "localhost"}:${MONGODB_PORT ?? 26182}/`,
-    {
-      auth: {
-        username: MONGODB_USER,
-        password: MONGODB_PW,
-      } as Auth,
-    }
-  );
-  return await client.connect();
+	const { MONGODB_PORT, MONGODB_USER, MONGODB_PW } = process.env;
+	const client = new MongoClient(`mongodb://localhost:${MONGODB_PORT}/`, {
+		auth: {
+			username: MONGODB_USER,
+			password: MONGODB_PW,
+		} as Auth,
+	});
+	return await client.connect();
 };
 
 @Service()
 export class DbClient {
-  private connection!: MongoClient;
+	private connection!: MongoClient;
 
-  async client(): Promise<MongoClient> {
-    if (!this.connection) {
-      this.connection = await createConnection();
-      try {
-        await this.connection.connect();
-      } catch (e) {
-        console.error(e);
-        throw e;
-      }
-    }
-    return this.connection;
-  }
+	async client(): Promise<MongoClient> {
+		if (!this.connection) {
+			this.connection = await createConnection();
+			try {
+				await this.connection.connect();
+			} catch (e) {
+				console.error(e);
+				throw e;
+			}
+		}
+		return this.connection;
+	}
 
-  async db(): Promise<Db> {
-    const client = await this.client();
-    return client.db("recapp");
-  }
+	async db(): Promise<Db> {
+		const client = await this.client();
+		return client.db("recapp");
+	}
 
-  shutdown(): Promise<void> {
-    return this.client().then((c) => c.close());
-  }
+	shutdown(): Promise<void> {
+		return this.client().then(c => c.close());
+	}
 }
